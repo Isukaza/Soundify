9ee2a6c6e882492a05a06a56c452ad1447d32992
Max Kakunia <maksimkharkov54@gmail.com>
2025-06-06 13:24:21 +0300

    Added Duration field to track update

--- Soundify/Models/Request/Update/TrackUpdateRequest.cs
+++ Soundify/Models/Request/Update/TrackUpdateRequest.cs
@@ -6,9 +6,12 @@ public class TrackUpdateRequest
 {
     [Required]
     public Guid Id { get; init; }
-    
+
     [StringLength(100)]
-    public  string Title { get; init; }
+    public string Title { get; init; }
+
+    [Range(0, 1800)]
+    public int Duration { get; init; }
 
     public DateTime? ReleaseDate { get; init; }
 }
